87267f090fc75eab625925820cd085f0e1d283fc
wasabi2710 <em.vannin.2001@gmail.com>
2025-03-31 13:39:56 +0700

    too lazy now

--- src/win32/main.c
+++ src/win32/main.c
@@ -1,6 +1,16 @@
+#include <winsock2.h>
 #include <stdio.h>
 
+#define SERVER_PORT 8000
+#define BINDER '0.0.0.0'
+
 int main() {
-    printf("Hi");
+    
+    WSADATA wsaData = {0};
+    SOCKET socket = 0;
+
+    
+    
+
     return 0;
 }
